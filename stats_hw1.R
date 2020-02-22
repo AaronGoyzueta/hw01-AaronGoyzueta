5d77640117747bec1bf5b1c5690ab7a3653653e1
Aaron Goyzueta <aarongoyzueta@gmail.com>
2020-02-22 12:29:53 -0500

    added code for part 2 (w/out soluations or stretch goal

--- stats_hw1.R
+++ stats_hw1.R
@@ -9,3 +9,14 @@ a <- 2
 b <- 4
 c <- -4
 (-b + sqrt((b^2) - 4*a*c)/2*a)
+
+# Part 2
+df <- read.csv("NYC.csv", header = TRUE)
+df_fourth <- df[df$word=="fouRth", ]
+xtabs(~r + emphasis + store, data=df_fourth)
+# Answer: Klein's employees used 
+
+df_klein <- df[df$store=="Klein's", ]
+df_klein_floor <- df_klein[df_klein$word=="flooR", ]
+floor_xtabs <- xtabs(~r, data=df_klein_floor)
+floor_xtabs/sum(floor_xtabs)
